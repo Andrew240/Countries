86cd6176f1fff035e724a62a4210727286660388
Andrew240 <momo7153@gmail.com>
2022-03-16 09:56:02 -0600

    Fixed issue about footer postion and update dom of datatable

--- src/app/views/table/table.component.ts
+++ src/app/views/table/table.component.ts
@@ -35,7 +35,10 @@ export class TableComponent implements AfterViewInit, OnDestroy, OnInit {
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 10,
-      lengthMenu: [[10], [10]],
+      lengthMenu: [[10, 50, 100, 250], [10, 50, 100, 250]],
+      dom: "<'row'<'col-lg-6 col-md-6 col-sm-6 col-12'l><'col-lg-6 col-md-6 col-sm-6 col-12'f>>" +
+          "<'row'<'col-12'tr>>" +
+          "<'row'<'col-lg-3 col-12'i><'col-lg-9 col-12'p>>",
       responsive: true,
       /* below is the relevant part, e.g. translated to spanish */
       language: {
